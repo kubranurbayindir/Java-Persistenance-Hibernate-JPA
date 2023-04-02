8dca279252c49c375884770ac135b2e01a0769d4
Kübra Nur BAYINDIR <83094285+kubranurbayindir@users.noreply.github.com>
2023-04-03 00:41:59 +0300

    Update Message.java

--- hibernate-as-jpa-provider/src/entity/Message.java
+++ hibernate-as-jpa-provider/src/entity/Message.java
@@ -10,15 +10,10 @@ import javax.persistence.Table;
 @Entity
 public class Message {
 
-
-	//for Hibernate 4.3.x Users
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO) 
 	private Long id;
 	
-	// for Hibernate 5.x Users
-	// if you're using Hibernate 5.x, use GenerationType.IDENTITY id generator strategy explicitly
-	// for more information on "GenerationType" have a look at https://www.udemy.com/hibernate-and-jpa-fundamentals/learn/v4/questions/937412
 	/*
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY) 
